66a128971a711668f08a68a794b185abf70dc079
jpprguezz <josepenrod@gmail.com>
2024-09-25 14:52:02 +0200

    Ejercicio circle-area hecho

---
+++ Números/circle-area/main.py
@@ -0,0 +1,11 @@
+def run(radius) -> float:
+    PI = 3.14
+    area = PI * (radius**2)
+    return area
+
+
+# DO NOT TOUCH THE CODE BELOW
+if __name__ == "__main__":
+    import vendor
+
+    vendor.launch(run)
