bb5266cc3edf5069761f7e8c7bcf534a8d4a45d1
jpprguezz <josepenrod@gmail.com>
2024-09-30 09:48:22 +0100

    Terminado ejercicio interest-rate

--- Números/interest-rate/main.py
+++ Números/interest-rate/main.py
@@ -1,5 +1,5 @@
 def run(amount: float, rate: float, years: int) -> float:
-    future_amount = amount * (1 + (1 / 100)) ** years
+    future_amount = amount * (1 + (rate / 100)) ** years
     return future_amount
 
 
