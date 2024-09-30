0be5dae5fc94c713d96cfc34606144da52b51307
jpprguezz <josepenrod@gmail.com>
2024-09-30 09:46:31 +0100

    Terminado ejercicio century-year

---
+++ Números/red-square/main.py
@@ -0,0 +1,11 @@
+def run(arc_A: float) -> float:
+    PI = 3.14
+    area = 
+    return area
+
+
+# DO NOT TOUCH THE CODE BELOW
+if __name__ == "__main__":
+    import vendor
+
+    vendor.launch(run)
