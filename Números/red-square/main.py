97373534d429d11c03ccdb6b07c7214078a2a4c5
jpprguezz <josepenrod@gmail.com>
2024-09-30 09:50:23 +0100

    Terminado ejercicio red-square

--- Números/red-square/main.py
+++ Números/red-square/main.py
@@ -1,7 +1,9 @@
 def run(arc_A: float) -> float:
     PI = 3.14
-    area = 
-    return area
+    circle_diameter = (arc_A * 4) / PI
+    radius = circle_diameter / 2
+    area = radius * radius
+    return round(area, 10)
 
 
 # DO NOT TOUCH THE CODE BELOW
