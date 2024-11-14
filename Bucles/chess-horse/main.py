bb65e90562a8328cc51931441882d1d22e49cca8
José Manuel Peña Rodríguez <145053972+jpprguezz@users.noreply.github.com>
2024-11-14 12:00:14 +0100

    Update main.py

--- Bucles/chess-horse/main.py
+++ Bucles/chess-horse/main.py
@@ -1,18 +1,5 @@
 def run(target_x: int, target_y: int) -> int:
-    x_movements = 0
-    y_movements = 0
-    turns = 0
-    while x_movements < target_x and y_movements < target_y:
-        x_movements += 2
-        y_movements += 2
-        turns += 1
-        x_movements += 1
-        y_movements += 2
-        turns += 1
-        if x_movements == target_x and y_movements == target_y:
-            movements = turns
-        elif target_x and target_y not in range(0, 10):
-            movements = -1
+
     return movements
 
 
