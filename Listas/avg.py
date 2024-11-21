198da9935c22fe17c1d92346b77483f8d82c8c0e
jpprguezz <josepenrod@gmail.com>
2024-11-21 19:15:06 +0100

    Ejercicio comentado

--- Listas/avg.py
+++ Listas/avg.py
@@ -1,6 +1,6 @@
 import sys
 
-values = sys.argv[1:]
-values_to_int = [int(v) for v in values]
-result = sum(values_to_int) / len(values_to_int)
-print(f'{result:.2f}')
+values = sys.argv[1:]  # El argumento dado en forma de string
+values_to_int = [int(v) for v in values]  # Pasamos a entero cada elemento del string, y gracias a la compresión, se queda automaticamente guardado en esta lista
+result = sum(values_to_int) / len(values_to_int)  # Calculamos el promedio
+print(f'{result:.2f}')  # Lo representamos con dos decimales
--- Listas/fcomp/main.py
+++ Listas/fcomp/main.py
@@ -1,5 +1,5 @@
 def run(xmin: int, xmax: int) -> list:
-    values = [(3 * num + 2) for num in range(xmin, xmax + 1)]
+    values = [(3 * num + 2) for num in range(xmin, xmax + 1)]  # La operación que deseamos hacer va al inicio del todo y el bucle con range recorre los numeros entre ambos numeros
     return values
 
 
